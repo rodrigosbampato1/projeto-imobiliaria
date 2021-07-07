23b810073e80257552ec90182722092c49193681
Thiago Gonçalves de Jesus <thiagogdejesus@outlook.com.br>
2021-07-07 08:48:56 -0300

    adicionado button go na barra de menun

---
+++ Exercicio Entrega Soul House 2.0/script.js
@@ -0,0 +1,10 @@
+//Aa+ Aa-
+tamanho = 10;
+function diminuir() {
+    tamanho --;
+    document.body.style.fontSize = tamanho +"px";
+}
+function aumentar() {
+    tamanho ++;
+    document.body.style.fontSize = tamanho +"px";
+}
